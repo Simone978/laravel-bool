a05197ed43006cdc8ac72b68afa78331a0121b3c
Simone <simonelega.sl@gmail.com>
2020-03-17 16:57:23 +0100

    Inserito controllo per distinguere maschi e femmine

--- app/Http/Controllers/DynamicPageController.php
+++ app/Http/Controllers/DynamicPageController.php
@@ -26,7 +26,8 @@ class DynamicPageController extends Controller
                 'eta' => '25 anni',
                 'azienda' => 'DISC SPA',
                 'ruolo' => 'web developer',
-                'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+                'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.',
+                'genere' => 'm'
             ],
             [
                 'poster' => 'https://www.boolean.careers/images/students/poggini.png',
@@ -34,7 +35,8 @@ class DynamicPageController extends Controller
                 'eta' => '24 anni',
                 'azienda' => 'Prima Assicurazioni ',
                 'ruolo' => 'junior software engineer',
-                'descrizione' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.'
+                'descrizione' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.',
+                'genere' => 'f'
             ],
             [
                 'poster' => 'https://www.boolean.careers/images/students/masetti.png',
@@ -42,7 +44,8 @@ class DynamicPageController extends Controller
                 'eta' => '36 anni',
                 'azienda' => 'Zen Agency',
                 'ruolo' => 'web developer',
-                'descrizione' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.'
+                'descrizione' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.',
+                'genere' => 'f'
             ]
         ];
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
    @foreach ($students as $student)
         <img src="{{$student['poster']}}" alt="{{$student['nome']}}">
        <h3>{{$student['nome']}} ({{$student['eta']}})</h3>
-       <h3>Assunto da {{$student['azienda']}} come {{$student['ruolo']}}</h3>
+       <h3>Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}</h3>
        <p>{{$student['descrizione']}}</p>
    @endforeach
 @endsection
