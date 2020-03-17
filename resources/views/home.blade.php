db67c2bd99a6636b0bb2d35b8bad16e16d1b6722
Simone <simonelega.sl@gmail.com>
2020-03-17 16:49:35 +0100

    Stampati i dati (senza formattazione)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,12 @@
 @extends('layouts.homeLayout')
 
-@dd($students)
+{{-- @dd($students) --}}
+
+@section('main')
+   @foreach ($students as $student)
+        <img src="{{$student['poster']}}" alt="{{$student['nome']}}">
+       <h3>{{$student['nome']}} ({{$student['eta']}})</h3>
+       <h3>Assunto da {{$student['azienda']}} come {{$student['ruolo']}}</h3>
+       <p>{{$student['descrizione']}}</p>
+   @endforeach
+@endsection
